debc7d6e38be0253da98f79f4752459105a3efd3
Sheru <100682913+SheruShafiq@users.noreply.github.com>
2024-02-08 14:09:46 +0100

    added effects and navigation

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,8 +1,11 @@
 import "./CustomButton.css";
 
+import { useNavigate } from "react-router-dom";
+
 function CustomButton() {
+  const navigate = useNavigate();
   return (
-    <button className="btn-class-name">
+    <button className="btn-class-name" onClick={() => navigate("/services")}>
       <span className="back"></span>
       <span className="front"></span>
     </button>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,12 @@
 import "../pageStyles/Home.css";
 
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 import AnimatedShip from "../components/AnimatedShip";
 import Bubbles from "../components/Bubbles";
-import CustomButton from "../components/CustomButton";
-import DayProgam from "../assets/Day-Program.png";
 import GIF from "../assets/bac.gif";
-import Inclusive from "../assets/Inclusive School Holiday Programs - image.png";
 import NavBar from "../components/NavBar";
-import SupportCoordination from "../assets/Support-Coordination.png";
-import SupportWork from "../assets/supportWork.png";
-import devLearning from "../assets/Play-based-developmental-learning-1.png";
-import employmentLvl from "../assets/Employment-ready-skills-training.png";
+import ServicesComponent from "../components/ServicesComponent";
 
 function Home() {
   return (
@@ -88,162 +82,7 @@ function Home() {
         </Box>
         <AnimatedShip />
       </Box>
-      <Stack alignSelf={"center"} gap={10} mt={"15rem"} position={"relative"}>
-        <Box display={"flex"} gap={20} alignSelf={"center"}>
-          <Stack mt={"-15rem"} justifyContent={"center"}>
-            <Box
-              component="img"
-              sx={{
-                height: "240px",
-                width: "240px",
-                bottom: "-35px",
-                zIndex: 0,
-              }}
-              position={"relative"}
-              alt="Day Programs"
-              src={DayProgam}
-            />
-            <CustomButton />
-            <Typography
-              fontSize={"2rem"}
-              mt={"1rem"}
-              ml={"-1rem"}
-              fontFamily="Gloria Hallelujah"
-            >
-              Day Programs
-            </Typography>
-          </Stack>
-
-          <Stack mt={"-8rem"} justifyContent={"center"} width={"210px"}>
-            <Box
-              component="img"
-              sx={{
-                height: "240px",
-                width: "340px",
-                bottom: "-35px",
-                right: "80px",
-                zIndex: 0,
-              }}
-              position={"relative"}
-              alt="Support Work"
-              src={SupportWork}
-            />
-            <CustomButton />
-            <Typography
-              fontSize={"2rem"}
-              mt={"1rem"}
-              ml={"-1rem"}
-              fontFamily="Gloria Hallelujah"
-              width={"100%"}
-            >
-              Support Work
-            </Typography>
-          </Stack>
-          <Stack mt={"-8rem"} justifyContent={"center"} width={"220px"}>
-            <Box
-              component="img"
-              sx={{
-                height: "160px",
-                width: "240px",
-                bottom: "35px",
-                right: "40px",
-                zIndex: 0,
-              }}
-              position={"relative"}
-              alt="Support Coordination"
-              src={SupportCoordination}
-            />
-            <CustomButton />
-            <Typography
-              fontSize={"2rem"}
-              mt={"1rem"}
-              ml={"-1rem"}
-              fontFamily="Gloria Hallelujah"
-              textAlign={"center"}
-            >
-              Support Coordination
-            </Typography>
-          </Stack>
-        </Box>
-        <Box display={"flex"} gap={30} mt={"5rem"}>
-          <Stack justifyContent={"center"} width={"220px"} mt="-8rem">
-            <Box
-              component="img"
-              sx={{
-                height: "280px",
-                width: "340px",
-                right: "5rem",
-                zIndex: 0,
-                bottom: "-40px",
-              }}
-              position={"relative"}
-              alt="Play-Based Developmental Learning"
-              src={devLearning}
-            />
-            <CustomButton />
-            <Typography
-              fontSize={"2rem"}
-              fontFamily="Gloria Hallelujah"
-              textAlign={"center"}
-              ml={"-3rem"}
-              mt={"1rem"}
-            >
-              Play-Based Developmental Learning
-            </Typography>
-          </Stack>
-
-          <Stack justifyContent={"center"} width={"220px"} mt="-8rem">
-            <Box
-              component="img"
-              sx={{
-                height: "280px",
-                width: "340px",
-                right: "5rem",
-                zIndex: 0,
-                bottom: "-40px",
-              }}
-              position={"relative"}
-              alt="Inclusive School Holiday Programs"
-              src={Inclusive}
-            />
-            <CustomButton />
-            <Typography
-              fontSize={"2rem"}
-              fontFamily="Gloria Hallelujah"
-              textAlign={"center"}
-              ml={"-3rem"}
-              mt={"1rem"}
-            >
-              Inclusive School Holiday Programs
-            </Typography>
-          </Stack>
-          <Stack justifyContent={"center"} width={"220px"} mt="-8rem">
-            <Box
-              component="img"
-              sx={{
-                height: "280px",
-                width: "340px",
-                right: "5rem",
-                zIndex: 0,
-                bottom: "-40px",
-              }}
-              position={"relative"}
-              alt="Employment ready skills training"
-              src={employmentLvl}
-            />
-            <CustomButton />
-            <Typography
-              fontSize={"2rem"}
-              fontFamily="Gloria Hallelujah"
-              textAlign={"center"}
-              ml={"-3rem"}
-              mt={"1rem"}
-            >
-              Employment Ready Skills Training
-            </Typography>
-          </Stack>
-        </Box>
-      </Stack>
+      <ServicesComponent />
       <Bubbles />
     </Box>
   );
